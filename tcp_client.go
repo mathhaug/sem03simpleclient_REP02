b58acfd709980e940606c9fd2bf41e04be6aba00
Mathias Haugen <haugen1997@gmail.com>
2023-04-20 17:26:17 +0000

    hjelp

--- tcp_client.go
+++ tcp_client.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"net"
+	"fmt"
 	"log"
+	"net"
 	"os"
+
+	"github.com/mathhaug/is105sem03_REP03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: tcp_client <message>")
+		os.Exit(1)
+	}
+
+	conn, err := net.Dial("tcp", "172.17.0.2:5002")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	log.Println("Din melding: ", os.Args[1])
+
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert sendt melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +32,14 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	switch response {
+	case "":
+		log.Println("response from proxy: ", response)
+	default:
+		response := mycrypt.Krypter(([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+		log.Println("response from proxy:  ", string(response))
+	}
 }
+
